5518064f37f7e10ea414d115b04c4f530766bc27
DavidEsdrs <carpina1983@gmail.com>
2023-06-07 05:39:24 -0300

    feat: Add Links navigation to navigate inside the application

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,27 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
     return (
-        <header className="bg-green-200 p-4">
+        <header className="bg-green-200 p-4 flex gap-4">
             <h1>
-                POLL.IT
+                <Link to={"/"}>
+                    POLL.IT
+                </Link>
             </h1>
+            <nav className="flex-1 flex justify-center font-semibold">
+                <ul className="flex gap-20">
+                    <li>
+                        <Link to={"/"}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={"/polls/create"}>
+                            Create poll
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </header>
     )
 }
